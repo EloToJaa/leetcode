70e37ba09ee4ebaf6001da41f080f2e327d939cc
EloToJaa <elotoja@protonmail.com>
2025-01-01 21:39:22 +0100

    Solve 380

---
+++ 274/solve.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int hIndex(vector<int>& citations) {}
+};
+
+int main() {
+  Solution solution;
+  vector<int> citations = {3, 0, 6, 1, 5};
+  return 0;
+}
