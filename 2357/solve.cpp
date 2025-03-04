151ed4c776c47e628bd84a6093030fd7ea8be08e
EloToJaa <elotoja@protonmail.com>
2025-03-04 21:12:41 +0100

    Solve 2357

---
+++ 2357/solve.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+ public:
+  int minimumOperations(vector<int>& nums) {
+    unordered_map<int, bool> used;
+    int ans = 0;
+    for (const int& num : nums) {
+      if (used[num] == true || num == 0) continue;
+      ans++;
+      used[num] = true;
+    }
+    return ans;
+  }
+};
+
+int main() {
+  Solution solution;
+  vector<int> nums = {1, 5, 0, 4, 5};
+  cout << solution.minimumOperations(nums);
+}
