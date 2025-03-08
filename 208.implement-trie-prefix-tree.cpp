30cac2b3aa0d07dd3c68d2b192f1b28abef429f9
EloToJaa <elotoja@protonmail.com>
2025-03-08 12:41:08 +0100

    Solve 208

--- 208.implement-trie-prefix-tree.cpp
+++ 208.implement-trie-prefix-tree.cpp
@@ -51,15 +51,52 @@
 
 using namespace std;
 
+class TrieNode {
+ public:
+  int nextNode[26];
+  bool isEnd;
+  TrieNode() : isEnd(false) { memset(nextNode, -1, sizeof(nextNode)); }
+};
+
 class Trie {
  public:
-  Trie() {}
+  vector<TrieNode> trie;
 
-  void insert(string word) {}
+  Trie() { trie.push_back(TrieNode()); }
 
-  bool search(string word) {}
+  void insert(string word) {
+    int node = 0;
+    for (const char& c : word) {
+      if (trie[node].nextNode[c - 'a'] == -1) {
+        trie[node].nextNode[c - 'a'] = trie.size();
+        trie.push_back(TrieNode());
+      }
+      node = trie[node].nextNode[c - 'a'];
+    }
+    trie[node].isEnd = true;
+  }
 
-  bool startsWith(string prefix) {}
+  bool search(string word) {
+    int node = 0;
+    for (const char& c : word) {
+      if (trie[node].nextNode[c - 'a'] == -1) {
+        return false;
+      }
+      node = trie[node].nextNode[c - 'a'];
+    }
+    return trie[node].isEnd;
+  }
+
+  bool startsWith(string prefix) {
+    int node = 0;
+    for (const char& c : prefix) {
+      if (trie[node].nextNode[c - 'a'] == -1) {
+        return false;
+      }
+      node = trie[node].nextNode[c - 'a'];
+    }
+    return true;
+  }
 };
 
 /**
@@ -69,4 +106,3 @@ class Trie {
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
-int main() { Solution solution; }
