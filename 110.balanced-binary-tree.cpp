5dc8ddb68d7707ac55b8c74336705bc8ca840d54
EloToJaa <elotoja@protonmail.com>
2026-07-22 02:00:48 +0200

    Solve 110

---
+++ 110.balanced-binary-tree.cpp
@@ -0,0 +1,71 @@
+// Category: algorithms
+// Level: Easy
+// Percent: 58.707737%
+
+// Given a binary tree, determine if it is height-balanced.
+//
+//  
+// Example 1:
+//
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+//
+//
+// Example 2:
+//
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+//
+//
+// Example 3:
+//
+// Input: root = []
+// Output: true
+//
+//
+//  
+// Constraints:
+//
+//
+// 	The number of nodes in the tree is in the range [0, 5000].
+// 	-10⁴ <= Node.val <= 10⁴
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ private:
+  bool isBalancedHelper(TreeNode *node, int &height) {
+    if (node == nullptr) {
+      height = -1;
+      return true;
+    }
+
+    int l, r;
+    if (isBalancedHelper(node->left, l) && isBalancedHelper(node->right, r) &&
+        abs(l - r) < 2) {
+      height = max(l, r) + 1;
+      return true;
+    }
+    return false;
+  }
+
+ public:
+  bool isBalanced(TreeNode *root) {
+    int height;
+    return isBalancedHelper(root, height);
+  }
+};
