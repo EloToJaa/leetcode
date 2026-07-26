02ef056394b32b8c9de037f29f5894624d6533e7
EloToJaa <elotoja@protonmail.com>
2026-07-26 23:03:40 +0200

    Solve 1703

---
+++ 1703.minimum-adjacent-swaps-for-k-consecutive-ones.cpp
@@ -0,0 +1,62 @@
+// Category: algorithms
+// Level: Hard
+// Percent: 42.383938%
+
+// You are given an integer array, nums, and an integer k. nums comprises of
+// only 0's and 1's. In one move, you can choose two adjacent indices and swap
+// their values.
+//
+// Return the minimum number of moves required so that nums has k consecutive
+// 1's.
+//
+//  
+// Example 1:
+//
+// Input: nums = [1,0,0,1,0,1], k = 2
+// Output: 1
+// Explanation: In 1 move, nums could be [1,0,0,0,1,1] and have 2 consecutive
+// 1's.
+//
+//
+// Example 2:
+//
+// Input: nums = [1,0,0,0,0,0,1,1], k = 3
+// Output: 5
+// Explanation: In 5 moves, the leftmost 1 can be shifted right until nums =
+// [0,0,0,0,0,1,1,1].
+//
+//
+// Example 3:
+//
+// Input: nums = [1,1,0,1], k = 2
+// Output: 0
+// Explanation: nums already has 2 consecutive 1's.
+//
+//
+//  
+// Constraints:
+//
+//
+// 	1 <= nums.length <= 10⁵
+// 	nums[i] is 0 or 1.
+// 	1 <= k <= sum(nums)
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int minMoves(vector<int>& nums, int k) {
+    vector<long long> A, B(1);
+    for (int i = 0; i < nums.size(); i++)
+      if (nums[i]) A.push_back(i);
+    long long n = A.size(), res = 2e9;
+    for (int i = 0; i < n; i++) B.push_back(B[i] + A[i]);
+    for (int i = 0; i < n - k + 1; i++)
+      res = min(res, B[i + k] - B[k / 2 + i] - B[(k + 1) / 2 + i] + B[i]);
+    res -= (long long)(k / 2) * ((k + 1) / 2);
+    return max(res, 0ll);
+  }
+};
